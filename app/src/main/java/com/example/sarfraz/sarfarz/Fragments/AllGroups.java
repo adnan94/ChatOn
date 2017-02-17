0c4884975e1f97a7b8de3e76bae29ba9217ece88
adnan94 <adnan.ahmed94k@gmail.com>
2017-02-17 11:08:56 -0800

    commit

--- app/src/main/java/com/example/sarfraz/sarfarz/Fragments/AllGroups.java
+++ app/src/main/java/com/example/sarfraz/sarfarz/Fragments/AllGroups.java
@@ -1,16 +1,22 @@
 package com.example.sarfraz.sarfarz.Fragments;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.sarfraz.sarfarz.Group;
 import com.example.sarfraz.sarfarz.MyGroupListAdaptor;
 import com.example.sarfraz.sarfarz.R;
+import com.example.sarfraz.sarfarz.Utils;
+import com.example.sarfraz.sarfarz.signature_friend_req;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,6 +25,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,6 +52,8 @@ public class AllGroups extends Fragment {
         listView=(ListView)v.findViewById(R.id.allGroups);
         list=new ArrayList<>();
         adaptor=new MyGroupListAdaptor(list,getActivity());
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        listView.setStackFromBottom(true);
         listView.setAdapter(adaptor);
 
 
@@ -68,7 +78,41 @@ public class AllGroups extends Fragment {
             }
         });
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
 
+
+                final AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+                alert.setPositiveButton("Join Group", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        Map<String, String> map = new HashMap<String, String>();
+                        map.put("name", list.get(position).getName());
+                        map.put("picurl", "N/A");
+                        map.put("admin", list.get(position).getAdmin());
+
+                        firebase.child("AppData").child("Groups").child("MyGroup").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child(list.get(position).getName()).setValue(map);
+
+                    }
+                });
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                dialog.dismiss();
+                    }
+                });
+                alert.setTitle("Group");
+                alert.setMessage("Are you sure you want to Join ?");
+                AlertDialog ad = alert.create();
+
+                alert.show();
+
+
+
+
+            }});
 
         return v;
     }
